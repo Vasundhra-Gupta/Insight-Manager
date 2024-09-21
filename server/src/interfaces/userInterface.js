9f786f22ad9a7eec399c071f2008c6f7a21e1de7
vasundhra <vasundhragupta962@gmail.com>
2024-09-22 00:50:25 +0530

    serviceObjects.js, userModel.js, userController.js, userInterface.js

---
+++ server/src/interfaces/userInterface.js
@@ -0,0 +1,38 @@
+export class Iusers {
+    async getUsers() {
+        throw new Error("Method getUsers not overwritten.");
+    }
+    async getUser(id) {
+        throw new Error("Method getUser not overwritten.");
+    }
+    async deleteUsers() {
+        throw new Error("Method deleteUsers not overwritten.");
+    }
+    async deleteUser(id) {
+        throw new Error("Method deleteUser not overwritten.");
+    }
+    async createUser(
+        id,
+        name,
+        firstName,
+        lastName,
+        avatar,
+        coverimage,
+        email,
+        password
+    ) {
+        throw new Error("Method createUsers not overwritten.");
+    }
+    async editUser(
+        id,
+        name,
+        firstName,
+        lastName,
+        avatar,
+        coverimage,
+        email,
+        password
+    ) {
+        throw new Error("Method editUser not overwritten.");
+    }
+}
