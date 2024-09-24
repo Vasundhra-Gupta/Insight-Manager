bd83960931f6dc12f173ab517144b2884fba86c1
vasundhra <vasundhragupta962@gmail.com>
2024-09-25 03:26:16 +0530

    added postModel.js

--- server/src/models/postModel.js
+++ server/src/models/postModel.js
@@ -1,18 +1,113 @@
 import { Iposts } from "../interfaces/postInterface.js";
 import { connection } from "../server.js";
-
+import validator from "validator";
+//views and time left
 export class SQLposts extends Iposts {
-    async getRandomPosts() {}
+    async getRandomPosts() {
+        try {
+            const q =
+                "SELECT  u.user_avatar, u.user_name, u.user_firstName, u.user_lastName, p.post_title, p.post_content, p.post_image FROM posts p, users u WHERE p.post_owner_id = u.user_id";
+            const [posts] = await connection.query(q);
+            if (!posts) {
+                return { message: "DB_POST_FETCH_FAILED" };
+            }
+            return posts;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async getPosts(userId) {}
+    async getPosts(userId) {
+        try {
+            // user_id conformation?
+            const q =
+                "SELECT  u.user_avatar, u.user_name, u.user_firstName, u.user_lastName, p.post_title, p.post_content, p.post_image FROM posts p, users u WHERE p.post_owner_id = u.user_id";
+            const [posts] = await connection.query(q, [userId]);
+            if (!posts) {
+                return { message: "POSTS_NOT_FOUND" };
+            }
+            return posts;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async getPost(postId) {}
+    async getPost(postId) {
+        try {
+            const q =
+                "SELECT  user_avatar, user_name , user_firstName, user_lastName, post_title, post_content, post_image FROM posts, users  WHERE post_id= ?";
+            const [[post]] = await connection.query(q, [postId]);
+            if (!post) {
+                return { message: "POST_NOT_FOUND" };
+            }
+            return post;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async deletePost(postId) {}
+    async deletePost(postId) {
+        try {
+            const q = "DELETE FROM posts WHERE post_id= ?";
+            const [result] = await connection.query(q, [postId]);
+            if (result.affectedRows == 0) {
+                throw new Error("POST_DELETION_DB_ISSUE");
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async updatePost(currentUserId, postId, title, content, image) {}
+    async updatePostDetails(postId, title, content, updatedAt) {
+        try {
+            const q = "UPDATE posts SET post_title = ?, post_content= ?, post_updatedAt= ? WHERE post_id = ?";
+            await connection.query(q, [title, content, updatedAt, postId]);
+            const post = await this.getPost(postId);
+            if (post?.message) {
+                throw new Error("POSTDETAILS_UPDATION_DB_ISSUE");
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async togglePostVisibility(PostId) {}
+    async updatePostImage(postId, image) {
+        try {
+            const q = "UPDATE posts SET post_image WHERE post_id = ?";
+            await connection.query(q, [image, postId]);
+            const post = await this.getPost(postId);
+            if (post?.message) {
+                throw new Error("POSTIMAGE_UPDATION_DB_ISSUE");
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 
-    async addPost(postId, ownerId, title, content, image) {}
+    async togglePostVisibility(postId, isVisible) {
+        try {
+            const q = "UPDATE posts SET post_visibility = ? WHERE post_id = ?";
+            await connection.query(q, [!isVisible, postId]);
+            const post = await this.getPost(postId);
+            if (post?.message) {
+                throw new Error("POSTVISIBILITY_UPDATION_DB_ISSUE");
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    async createPost(postId, ownerId, title, content, image) {
+        try {
+            const q = "INSERT INTO posts (post_id, post_owner_id, post_title, post_content, post_image) VALUES (?, ?, ?, ?, ?)";
+            await connection.query(q, [postId, ownerId, title, content, image]);
+            const post = await this.getPost(postId);
+            if (post?.message) {
+                throw new Error("POST_CREATION_DB_ISSUE");
+            }
+            return post;
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
 }
