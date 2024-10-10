c5a25dffc6a42519b13943ffd15da55dfd4ff4d1
Sania Singla <singlasania7717@gmail.com>
2024-10-11 00:40:13 +0530

    refinement

--- client/src/Utils/regex.js
+++ client/src/Utils/regex.js
@@ -1,48 +1,62 @@
 export default function verifyExpression(name, value, setError) {
     if (value) {
-        if (name === "email") {
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$/.test(value)
-                ? setError((prevError) => ({ ...prevError, [name]: "" }))
-                : setError((prevError) => ({
-                      ...prevError,
-                      [name]: "please enter a valid email.",
-                  }));
-        }
+        switch (name) {
+            case "email": {
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$/.test(value)
+                    ? setError((prevError) => ({ ...prevError, [name]: "" }))
+                    : setError((prevError) => ({
+                          ...prevError,
+                          [name]: "please enter a valid email.",
+                      }));
+                break;
+            }
 
-        if (name === "firstName" || name === "lastName") {
-            /^[a-zA-Z]{1,15}$/.test(value)
-                ? setError((prevError) => ({ ...prevError, [name]: "" }))
-                : setError((prevError) => ({
-                      ...prevError,
-                      [name]: `only letters are allowed and ${name} can be only 15 char long.`,
-                  }));
-        }
+            case "firstName":
+            case "lastName": {
+                /^[a-zA-Z]{1,15}$/.test(value)
+                    ? setError((prevError) => ({ ...prevError, [name]: "" }))
+                    : setError((prevError) => ({
+                          ...prevError,
+                          [name]: `only letters are allowed and ${name} can be only 15 char long.`,
+                      }));
+                break;
+            }
 
-        if (name === "userName") {
-            /^[a-zA-Z0-9_]{1,20}$/.test(value)
-                ? setError((prevError) => ({ ...prevError, [name]: "" }))
-                : setError((prevError) => ({
-                      ...prevError,
-                      [name]: "only numbers, letters and underscores are allowed and userName can be 20 char long",
-                  }));
-        }
+            case "userName": {
+                /^[a-zA-Z0-9_]{1,20}$/.test(value)
+                    ? setError((prevError) => ({ ...prevError, [name]: "" }))
+                    : setError((prevError) => ({
+                          ...prevError,
+                          [name]: "only numbers, letters and underscores are allowed and userName can be 20 char long",
+                      }));
+                break;
+            }
 
-        if (name === "password" || name === "newPassword") {
-            value.length >= 8 && value.length <= 12
-                ? setError((prevError) => ({ ...prevError, [name]: "" }))
-                : setError((prevError) => ({
-                      ...prevError,
-                      [name]: `${name} must be 8-12 characters.`,
-                  }));
-        }
+            case "password":
+            case "newPassword": {
+                value.length >= 8 && value.length <= 12
+                    ? setError((prevError) => ({ ...prevError, [name]: "" }))
+                    : setError((prevError) => ({
+                          ...prevError,
+                          [name]: `${name} must be 8-12 characters.`,
+                      }));
+                break;
+            }
 
-        if (name === "bio") {
-            value.length <= 100
-                ? setError((prevError) => ({ ...prevError, [name]: "" }))
-                : setError((prevError) => ({
-                      ...prevError,
-                      [name]: "Bio should not exceed 100 characters.",
-                  }));
+            case "bio": {
+                value.length <= 100
+                    ? setError((prevError) => ({ ...prevError, [name]: "" }))
+                    : setError((prevError) => ({
+                          ...prevError,
+                          [name]: "Bio should not exceed 100 characters.",
+                      }));
+                break;
+            }
+
+            default: {
+                console.log("Doesn't have a defined regex.");
+                return;
+            }
         }
     }
 }
