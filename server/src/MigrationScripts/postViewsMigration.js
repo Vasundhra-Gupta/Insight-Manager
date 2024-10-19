dce8d40f193b5af10916b5e56e7ca4cfed8b4770
vasundhra <vasundhragupta962@gmail.com>
2024-10-19 23:28:44 +0530

    updated postViewsmigration.js

--- server/src/MigrationScripts/postViewsMigration.js
+++ server/src/MigrationScripts/postViewsMigration.js
@@ -1,6 +1,6 @@
 import { connection } from "../server.js";
 import { SERVER_ERROR } from "../constants/errorCodes.js";
-import { SavedPost } from "../schemas/MongoDB/userSchema.js";
+import { PostView } from "../schemas/MongoDB/postSchema.js";
 
 export async function migratePostViews(req, res, next) {
     try {
@@ -9,7 +9,7 @@ export async function migratePostViews(req, res, next) {
         if (SQLviewedPost.length) {
             const SQLviewedPostKeys = SQLviewedPost.map((v) => `${v.post_id} ${v.user_identifier}`);
 
-            const [MongoDBviewedPost] = await SavedPost.find();
+            const [MongoDBviewedPost] = await PostView.find();
     
             const MongoDBviewedPostKeys = MongoDBviewedPost.map((v) => `${v.post_id} ${v.user_identifier}`);
 
@@ -22,13 +22,13 @@ export async function migratePostViews(req, res, next) {
                 }
             }
 
-            const deletedViewedPost = MongoDBpostViews.filter((v) => {
+            const deletedViewedPost = MongoDBviewedPost.filter((v) => {
                 const key = `${v.post_id} ${v.user_identifier}`;
                 return !SQLviewedPostKeys.includes(key);
             });
 
             if (newViewedPost.length) {
-                await SavedPost.insertMany(newViewedPost);
+                await PostView.insertMany(newViewedPost);
             }
 
             if (deletedViewedPost.length) {
@@ -36,14 +36,14 @@ export async function migratePostViews(req, res, next) {
                     post_id: v.post_id,
                     user_identifier: v.user_identifier,
                 }));
-                await SavedPost.deleteMany({ $or: deleteFilters });
+                await PostView.deleteMany({ $or: deleteFilters });
             }
 
             console.log(`${deletedViewedPost.length} new VIEWEDPOSTS INSERTED \n ${deletedViewedPost} VIEWEDPOSTS DELETED`);
         } else {
-            const count = SavedPost.countDocuments();
+            const count = PostView.countDocuments();
             if (count) {
-                await SavedPost.deleteMany();
+                await PostView.deleteMany();
                 console.log("CLEARED MONGODB VIEWEDPOSTS\n");
             } else {
                 console.log("NO_VIEWEDPOSTS_TO_MIGRATE");
