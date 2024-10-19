cf8a87edf51383fc2b2ba19cda18d3e09fbe9db2
vasundhra <vasundhragupta962@gmail.com>
2024-10-20 02:04:12 +0530

    updated changes

--- server/src/MigrationScripts/index.js
+++ server/src/MigrationScripts/index.js
@@ -4,8 +4,7 @@ import { migratePostLikes, migrateCommentLikes } from "./likesMigration.js";
 import { migrateComments } from "./commentsMigration.js";
 import { migrateCategories } from "./categoriesMigration.js";
 import { migratePosts } from "./postsMigration.js";
-import { migratePostLikes } from "./likesMigration.js";
-import { migrateCommentLikes } from "./likesMigration.js";
+
 export {
     migrateCategories,
     migrateComments,
